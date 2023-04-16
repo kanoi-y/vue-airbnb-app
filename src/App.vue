1f07f9d1f96d4fc467ba49827c8e25d9461506e6
kanoi-y <furafuratonbo@gmail.com>
2023-04-16 21:39:08 +0900

    ✨ todoを削除できるように実装

--- src/App.vue
+++ src/App.vue
@@ -18,21 +18,22 @@ const addTodo = () => {
   });
   newTodo.value = '';
 };
+const deleteTodo = (id: string) => {
+  todos.value = todos.value.filter((todo) => todo.id !== id);
+};
 </script>
 
 <template>
   <div class="p-4">
     <h1 class="text-3xl mb-8">Todo List</h1>
     <div class="flex flex-col gap-3 mb-5">
-      <label
-        v-for="todo in todos"
-        :key="todo.id"
-        :for="todo.id"
-        class="flex items-center gap-2 cursor-pointer"
-      >
-        <input type="checkbox" :id="todo.id" v-model="todo.done" />
-        <p :class="todo.done ? 'line-through' : ''">{{ todo.title }}</p>
-      </label>
+      <div v-for="todo in todos" :key="todo.id">
+        <label :for="todo.id" class="flex items-center gap-2 cursor-pointer">
+          <input type="checkbox" :id="todo.id" v-model="todo.done" />
+          <p :class="todo.done ? 'line-through' : ''">{{ todo.title }}</p>
+        </label>
+        <button type="button" @click="() => deleteTodo(todo.id)">X</button>
+      </div>
     </div>
     <div>
       <label><input type="text" v-model="newTodo" /></label>
